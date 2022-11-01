b2d6021bb36eca0ad4e3d6df0c68d2710be98fed
Juan Cassiano <juancassiano@hotmail.com>
2022-10-31 22:57:44 -0300

    Configuração inicial

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,9 @@ async function bootstrap(){
   const fastify = Fastify({
     logger: true,
   })
+  fastify.get('/pools/count', () =>{
+    return { count : 0 }
+  })
 
   await fastify.listen({ port: 3333 })
 } 
